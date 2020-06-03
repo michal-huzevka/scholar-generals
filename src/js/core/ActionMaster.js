06da56794624209a11a1b13fc3f6ec705430fdb4
Michal Huzevka <michal.huzevka583@gmail.com>
2020-06-03 10:37:06 +1000

    action master cleanup

--- src/js/core/ActionMaster.js
+++ src/js/core/ActionMaster.js
@@ -3,61 +3,64 @@ import GlobalView from 'js/core/views/GlobalView';
 import PlayersView from 'js/core/views/PlayersView';
 import Player from 'js/core/models/Player';
 
+const moveUnit = (action, state) => {
+    const { fromLocation, toLocation } = action.data;
+    const gridView = new GridView(state);
+    const playersView = new PlayersView(state);
+    let fromTile = gridView.getTile(fromLocation);
+    let toTile = gridView.getTile(toLocation);
+    let unit = gridView.getUnit(fromLocation);
+
+    if (unit.getOwner() !== playersView.getActivePlayerId()) {
+        return false;
+    }
+
+    unit = unit.spendMoves(gridView.getDistance(fromLocation, toLocation));
+    fromTile = fromTile.setUnitId(null);
+    toTile = toTile.setUnitId(unit.getId());
+
+    state = state
+        .setModel(unit)
+        .setModel(fromTile)
+        .setModel(toTile);
+
+    return {
+        state,
+        outcome: {}
+    };
+}
+
+const endTurn = (state) => {
+    const playersView = new PlayersView(state);
+    const globalView = new GlobalView(state);
+
+    const units = globalView.getAllUnitsForActivePlayer();
+
+    units.forEach((unit) => {
+        state = state.setModel(unit.refresh());
+    });
+    const activePlayerId = Player.getOpponentId(playersView.getActivePlayerId());
+
+    return {
+        state: state.set('activePlayerId', activePlayerId),
+        outcome: null
+    };
+}
+
 class ActionMaster {
     computeAction(action, state) {
-        let newState = state;
-        let outcome = {};
         if (action.type === 'MOVE_UNIT') {
-            const { fromLocation, toLocation } = action.data;
-
-            newState = this.moveUnit(fromLocation, toLocation, state);
+            return moveUnit(action, state);
         }
 
         if (action.type === 'END_TURN') {
-            newState = this.endTurn(state);
+            return endTurn(state);
         }
 
         return {
-            state: newState,
-            outcome: {}
+            state,
+            outcome: null
         }
-    }
-
-    moveUnit = (fromLocation, toLocation, state) => {
-        const gridView = new GridView(state);
-        const playersView = new PlayersView(state);
-        let fromTile = gridView.getTile(fromLocation);
-        let toTile = gridView.getTile(toLocation);
-        let unit = gridView.getUnit(fromLocation);
-
-        if (unit.getOwner() !== playersView.getActivePlayerId()) {
-            return false;
-        }
-
-        unit = unit.spendMoves(gridView.getDistance(fromLocation, toLocation));
-        fromTile = fromTile.setUnitId(null);
-        toTile = toTile.setUnitId(unit.getId());
-
-        state = state
-            .setModel(unit)
-            .setModel(fromTile)
-            .setModel(toTile);
-
-        return state;
-    }
-
-    endTurn = (state) => {
-        const playersView = new PlayersView(state);
-        const globalView = new GlobalView(state);
-
-        const units = globalView.getAllUnitsForActivePlayer();
-
-        units.forEach((unit) => {
-            state = state.setModel(unit.refresh());
-        });
-        const activePlayerId = Player.getOpponentId(playersView.getActivePlayerId());
-
-        return state.set('activePlayerId', activePlayerId);
     }
 }
 
