030f3ee9209d699e84750bed6b5b47adec5ead5b
Michal Huzevka <michal.huzevka583@gmail.com>
2020-06-06 22:59:44 +1000

    console log when attacking

--- src/js/game/ui/components/HexGrid.js
+++ src/js/game/ui/components/HexGrid.js
@@ -4,6 +4,7 @@ import HexGrid from 'js/game/utils/hexGrid/HexGrid';
 import withGlobalContext from 'js/game/ui/withGlobalContext';
 import Tile from 'js/game/ui/components/Tile';
 import GridView from 'js/game/core/views/GridView';
+import PlayersView from 'js/game/core/views/PlayersView';
 import UnitMovementView from 'js/game/core/views/UnitMovementView';
 import UnitAttackView from 'js/game/core/views/UnitAttackView';
 
@@ -54,25 +55,39 @@ class HexGridComponent extends React.Component {
         const gridView = new GridView(this.props.gameState);
         const tile = gridView.getTileView(location);
         const unit = tile.getUnit();
+        const { selectedLocation } = this.state;
         
         if (unit) {
-            const unitMovementView = new UnitMovementView(this.props.gameState, location);
             const unitAttackView = new UnitAttackView(this.props.gameState, location);
+            const attackableLocations = unitAttackView.getAttackableLocations();
+            const selectedUnit = selectedLocation && gridView.getUnit(selectedLocation);
 
-            this.setState({
-                selectedLocation: location,
-                locationsInRange: unitMovementView.getReachableLocations(),
-                attackableLocations: unitAttackView.getAttackableLocations()
-            });
-        } else if (this.state.selectedLocation) {
-            const unitMovementView = new UnitMovementView(this.props.gameState, this.state.selectedLocation);
+            if (
+                selectedLocation &&
+                _.find(attackableLocations, location => _.isEqual(location, selectedLocation)) &&
+                selectedUnit.getOwner() === this.props.activePlayerId
+            ) {
+                // if the unit can be attacked, attack it
+                console.log('attacking');
+            } else {
+                // if not, select the location
+                const unitMovementView = new UnitMovementView(this.props.gameState, location);
+
+                this.setState({
+                    selectedLocation: location,
+                    locationsInRange: unitMovementView.getReachableLocations(),
+                    attackableLocations: unitAttackView.getAttackableLocations()
+                });
+            }
+        } else if (selectedLocation) {
+            const unitMovementView = new UnitMovementView(this.props.gameState, selectedLocation);
 
             if (unitMovementView.isUnitInMoveRange(location)) {
                 // do a move
                 this.props.doAction({
                     type: 'MOVE_UNIT',
                     data: {
-                        fromLocation: this.state.selectedLocation,
+                        fromLocation: selectedLocation,
                         toLocation: location
                     }
                 });
@@ -97,6 +112,7 @@ class HexGridComponent extends React.Component {
 export default withGlobalContext(HexGridComponent, (coreInterface) => {
     return {
         gameState: coreInterface.getActiveState(),
+        activePlayerId: new PlayersView(coreInterface.getActiveState()).getActivePlayerId(),
         doAction: coreInterface.doAction
     };
 });
